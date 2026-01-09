facaad50519ab4d64980c7991d5605c32297f8ff
Giovanni Bordiga <gio.61192@gmail.com>
2026-01-09 18:04:19 -0500

    Minor reformat

--- tracking_markers/tracking_points.py
+++ tracking_markers/tracking_points.py
@@ -93,8 +93,7 @@ def track_points(
     cap = cv2.VideoCapture(str(video_path))
     frame_start, frame_end = frame_range
     frame_number = frame_start
-    frame_end = frame_end if frame_end > 0 else int(
-        cap.get(cv2.CAP_PROP_FRAME_COUNT))-1
+    frame_end = frame_end if frame_end > 0 else int(cap.get(cv2.CAP_PROP_FRAME_COUNT))-1
 
     if show_tracked_frame:
         cv2.namedWindow('Frame', cv2.WINDOW_NORMAL)
